b6497a67a9d47380c6b9d73f3655eabf40c91311
swheatley <shaylee1@me.com>
2016-10-29 22:48:55 -0600

    Exercises 2.8 -> 2.9

---
+++ numbers.py
@@ -0,0 +1,17 @@
+#2.8 Number Eight
+add_eight = 5 + 3
+subtract_eight = 18 - 10
+multiply_eight = 8*1
+divide_eight = 24/3
+print(add_eight)
+print(subtract_eight)
+print(multiply_eight)
+print(divide_eight)
+#
+#
+#2.9 Favorite Number
+favorite_number = 26
+message = "My favorite number is"  + " " +  str(favorite_number)
+print(message)
+#
+#
