8213dd2d749607beab78fd1ce4238fffb8f150b9
swheatley <shaylee1@me.com>
2016-10-29 22:34:23 -0600

    Exercises 2.3 -> 2.7 in Chapter 2

---
+++ name_cases.py
@@ -0,0 +1,31 @@
+#2.3 Personal Message
+
+first_name = "Alex"
+message1 = "you are awesome!"
+answer = first_name + " " + message1
+print(answer)
+#
+#
+#2.4 Name Cases
+his_name = "alex"
+print(his_name.title())
+print(his_name.lower())
+print(his_name.upper())
+#
+#
+#.2.5 Famous Quote
+famous_quote = '"Hardships often prepare ordinary people for an extraordinary destiny" -\nC.S. Lewis'
+print(famous_quote)
+#
+#
+#2.6 Famous Quote 2
+famous_person = "Maya Angelou"
+message = "If you don't like something, change it. If you can't change it, change your attitude."
+print(message + " " + famous_person)
+#
+#
+#2.7 Stripping Names
+new_name = "\tSh\tayl\teeHansen"
+print(new_name.rstrip())
+print(new_name.lstrip())
+print(new_name.strip())
