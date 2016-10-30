54a8b15709117aae6f4a4a02d2bcb09ca77daba0
swheatley <shaylee1@me.com>
2016-10-29 23:14:38 -0600

    Chapter 3 Exercises 3.1 -> 3.3

---
+++ lists.py
@@ -0,0 +1,25 @@
+#3.1 Names
+friends_list = ['Sophia', 'Elizabeth', 'Sean', 'Sarah', 'Alex']
+print(friends_list[0])
+print(friends_list[1])
+print(friends_list[2])
+print(friends_list[3])
+print(friends_list[-1])
+#
+#
+# 3.2 Greetings
+print(friends_list[0] + " " + "you are my Russian friend")
+print(friends_list[1] + " " + "you are my coding buddy")
+print(friends_list[2] + " " + "you are a former colleague")
+print(friends_list[3] + " " + "you are my sister")
+print(friends_list[-1]+ " " + "you are my loving husband")
+#
+#
+# 3.3 Your own List
+modes_of_transport = ['car', 'trax train', 'airplane', 'boat']
+print("My " + " " +  modes_of_transport[0] + " " + " is a Mazda3")
+print("Sometimes I like taking the " + " " + modes_of_transport[1].title())
+print("I've flown on a " + " " + modes_of_transport[2] + " " + " multiple times")
+print("I would like to own a" + " " + modes_of_transport[3])
+#
+#
